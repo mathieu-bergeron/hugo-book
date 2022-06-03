326a130497f4afe7191621e8e1fc0a10b2c5d54c
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2022-06-03 14:38:51 -0400

    TODO) utiliser /data pour écrire le contenu en .yaml

--- shortcodes_src/compile.sh
+++ shortcodes_src/compile.sh
@@ -1,7 +1,8 @@
-# compile simple means removing all spaces and newlines
+# compile simply means removing all spaces and newlines
 
 for i in *.html;
 do
+    #iconv  -f utf-8 -t ascii $i -o $i
     target=../layouts/shortcodes/$i
     cat $i | tr -d '\n' | sed "s/}[[:space:]]\+/}/g" > $target
 done
